0a724ba676b5de0a7b8ea83508299403ff60616f
fatawaimamalmuftin <fatawaimamalmuftin17@gmail.com>
2026-08-12 01:38:03 +0700

    feat: make navbar and use react-route to navigate

---
+++ src/Router.jsx
@@ -0,0 +1,24 @@
+// import React from 'react'
+
+import ClassComp from "./pages/ClassComp"
+import Counter from "./pages/Counter"
+import FuncComp from "./pages/FuncComp"
+import FetchPokemon from "./pages/FetchPokemon"
+import Navbar from "./components/Navbar"
+import LiftingState from "./pages/LiftingState"
+import {Routes,Route} from 'react-router'
+
+export default function Router(){
+  return(
+    <>
+    <Navbar/>
+    <Routes>
+      <Route path={'/pokemon'} element={<FetchPokemon/>} />
+      <Route path={'/classcomp'} element={<ClassComp/>} />
+      <Route path={'/funccomp'} element={<FuncComp/>} />
+      <Route path={'/counter'} element={<Counter/>} />
+      <Route path={'/LiftingState'} element={<LiftingState/>} />
+    </Routes>
+    </>
+  )
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,33 @@
+import { Link } from "react-router"
 
-export default function Navbar({logo}) {
+export default function Navbar() {
   return (
     <nav className=" border-b-2 border-purple-900 flex justify-between items-center px-4 py-2">
-        <div>{logo}</div>
+        <div>
+          <Link to={'/pokemon'}>
+            LOGO
+          </Link>
+        </div>
         <ul className="flex gap-5 px-2 py-2">
-            <li>Class Component</li>
-            <li>Funct Component</li>
-            <li>Counter</li>
-            <li>Pokemon</li>
+            <li>
+              <Link to={'/classcomp'}>
+                Class Component
+              </Link>              </li>
+            <li>
+              <Link to={'/funccomp'}>
+                Funct Component
+              </Link>
+            </li>
+            <li>
+              <Link to={'/counter'}>
+                Counter
+              </Link>
+            </li>
+            <li>
+              <Link to={'/LiftingState'}>
+                LiftState
+              </Link>
+            </li>
         </ul>
     </nav>
   )
