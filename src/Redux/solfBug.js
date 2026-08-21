dadf83c720aa8a836da069c7b459187707bfba97
fatawaimamalmuftin <fatawaimamalmuftin17@gmail.com>
2026-08-22 00:25:03 +0700

    feat: added bug redux persist

---
+++ src/Redux/solfBug.js
@@ -0,0 +1,13 @@
+const storage = {
+  getItem: (key) => {
+    return Promise.resolve(window.localStorage.getItem(key));
+  },
+  setItem: (key, value) => {
+    return Promise.resolve(window.localStorage.setItem(key, value));
+  },
+  removeItem: (key) => {
+    return Promise.resolve(window.localStorage.removeItem(key));
+  },
+};
+
+export default storage
